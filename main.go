af45ffbeb3b14bf703d851213f08c875dbbf2fca
drfaust92 <ilia.lazebnik@gmail.com>
2023-04-28 19:29:42 +0300

    debug

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"terraform-provider-schema-registry/schemaregistry"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -8,9 +9,16 @@ import (
 )
 
 func main() {
+	var debug bool
+
+	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() *schema.Provider {
 			return schemaregistry.Provider()
 		},
+		ProviderAddr: "registry.terraform.io/drfaust92/conflunet-schema-registry",
+		Debug:        debug,
 	})
 }
